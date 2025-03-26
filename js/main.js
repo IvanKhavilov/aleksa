f688029ed4fcdc517a073eb4649f7e53a1ea0613
IvanKhavilov <73675358+IvanKhavilov@users.noreply.github.com>
2025-03-26 11:58:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,72 +19,32 @@ function showModal() {
   modal.style.display = 'flex'
   setTimeout(() => {
     modal.style.display = 'none'
-  }, 5000)
+  }, 4000)
 }
 
-function enableSubmitBtn() {
-  document.querySelectorAll('[type="submit"]').forEach((btn) => {
-    btn.disabled = false // Разблокируем все кнопки отправки
-  })
-}
-function onloadCallback() {
-  renderRecaptchas()
-}
-function renderRecaptchas() {
-  if (typeof grecaptcha !== 'undefined') {
-    document.querySelectorAll('.g-recaptcha').forEach((recaptcha) => {
-      if (!recaptcha.hasAttribute('data-widget-id')) {
-        let widgetId = grecaptcha.render(recaptcha, {
-          sitekey: recaptcha.getAttribute('data-sitekey'),
-        })
-        recaptcha.setAttribute('data-widget-id', widgetId)
-      }
-    })
-  } else {
-    console.warn('grecaptcha не определён.')
-  }
-}
-document.addEventListener('DOMContentLoaded', renderRecaptchas)
-function resetAllRecaptchas() {
-  if (typeof grecaptcha !== 'undefined') {
-    document.querySelectorAll('.g-recaptcha').forEach((recaptcha) => {
-      let widgetId = recaptcha.getAttribute('data-widget-id')
-      if (widgetId) {
-        grecaptcha.reset(widgetId)
-      }
-    })
-  } else {
-    console.warn('grecaptcha не определён.')
-  }
-}
 function submitForm(selector) {
   let form = document.querySelector(selector)
   if (!form) return // Если форма не найдена, выходим
 
   form.addEventListener('submit', function (event) {
-    event.preventDefault()
+    event.preventDefault() // Останавливаем стандартную отправку
 
     let formData = new FormData(form)
     let submitBtn = form.querySelector('[type="submit"]')
-    submitBtn.disabled = true // Блокируем кнопку, чтобы избежать повторных кликов
 
     fetch(form.action, { method: form.method, body: formData })
       .then((response) => response.json()) // Ожидаем JSON-ответ
       .then((data) => {
         if (data.success === true) {
           form.reset() // Очищаем форму
-          // Сбрасываем все капчи
-          resetAllRecaptchas()
           showModal()
         } else {
           alert(data.error || 'Ошибка при отправке формы. Попробуйте ещё раз.')
         }
       })
       .catch((error) => console.error('Ошибка запроса:', error))
-      .finally(() => {
-        submitBtn.disabled = false // Разблокируем кнопку
-      })
   })
 }
 
+// Вызываем функцию для каждой формы
 submitForm('.page__form')
